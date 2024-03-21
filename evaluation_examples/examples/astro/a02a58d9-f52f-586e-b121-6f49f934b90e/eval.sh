aa62a797f01c967a24cdecb9f7683c67db7681a3
rhythmcao <ruishengcao@gmail.com>
2024-03-21 23:10:09 +0800

    fix bug in astro example

--- evaluation_examples/examples/astro/a02a58d9-f52f-586e-b121-6f49f934b90e/eval.sh
+++ evaluation_examples/examples/astro/a02a58d9-f52f-586e-b121-6f49f934b90e/eval.sh
@@ -4,24 +4,42 @@ source /home/user/anaconda3/etc/profile.d/conda.sh
 conda activate astro
 cd /home/user/projects/astro-jaffle-shop
 
-DAG_ID=jaffle_shop_dag
+astro dev start >/dev/null 2>/dev/null
+export DAG_ID=jaffle_shop_dag
+export CONTAINER=$(astro dev ps | grep "webserver" | awk '{print $1}')
 
-# check whether the DAG is written correctly
-astro run ${DAG_ID} >output.log 2>/dev/null
-output=$(tail -n 5 output.log)
-# TOTAL=10 is combined with jaffle_shop project
-flag1=$(echo $output | grep "PASS=10 .* ERROR=0 .* TOTAL=10")
-flag2=$(echo $output | grep "SUCCESS")
-if [ -n "$flag1" ] && [ -n "$flag2" ]; then
-    echo "astro run dag succeed"
+# check whether the DAG is jaffle_shop dbt project
+declare -a task_list=(
+    "customers.customers_run"
+    "customers.customers_test"
+    "orders.orders_run"
+    "orders.orders_test"
+    "raw_customers_seed"
+    "raw_orders_seed"
+    "raw_payments_seed"
+    "stg_customers.stg_customers_run"
+    "stg_customers.stg_customers_test"
+    "stg_orders.stg_orders_run"
+    "stg_orders.stg_orders_test"
+    "stg_payments.stg_payments_run"
+    "stg_payments.stg_payments_test"
+)
+jaffle_shop_tasks=$(docker exec -i $CONTAINER airflow tasks list ${DAG_ID})
+contains_all=true
+for task in "${task_list[@]}"; do
+    if [[ ! ${jaffle_shop_tasks} == *"$task"* ]]; then
+        contains_all=false
+    fi
+done
+if ${contains_all}; then
+    echo "jaffle_shop_dag succeed"
 else
-    echo "astro run dag failed"
+    echo "jaffle_shop_dag failed"
     exit 0
 fi
 
-astro dev start >/dev/null 2>/dev/null
-container=$(astro dev ps | grep webserver | awk '{print $1}')
-active_flag=$(docker exec -i $container airflow dags details -o plain ${DAG_ID} | grep is_active | grep -i true)
+# check whether the DAG is not paused
+active_flag=$(docker exec -i $CONTAINER airflow dags details -o plain ${DAG_ID} | grep "is_paused" | grep -i "false")
 if [ -n "$active_flag" ]; then
     echo "dag active succeed"
 else
@@ -29,7 +47,18 @@ else
     exit 0
 fi
 
-interval_flag=$(docker exec -i $container airflow dags details -o plain ${DAG_ID} | grep schedule_interval | grep '0 10 \* \* \*')
+# check whether the DAG can run successfully
+astro run ${DAG_ID} >/dev/null 2>/dev/null # manually run it
+flag=$(docker exec -i $CONTAINER airflow dags list-runs -o plain --dag-id ${DAG_ID} --no-backfill | grep "${DAG_id}" | grep -m 1 "manual" | awk '{print $3}')
+if [ "$flag" = "success" ]; then
+    echo "astro run dag succeed"
+else
+    echo "astro run dag failed"
+    exit 0
+fi
+
+# check whether the interval is set correctly
+interval_flag=$(docker exec -i $CONTAINER airflow dags details -o plain ${DAG_ID} | grep "schedule_interval" | grep "0 10 \* \* \*")
 if [ -n "$interval_flag" ]; then
     echo "dag schedule_interval succeed"
 else
