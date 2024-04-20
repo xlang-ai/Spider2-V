2f49e817ba064cdc642ae549476884f00c96290a
runoob <96536860+Hongcheng-Gao@users.noreply.github.com>
2024-04-20 21:59:49 +0800

    13d420b9-b685-4afb-ac98-997cbbd266d9

---
+++ evaluation_examples/examples/astro/13d420b9-b685-4afb-ac98-997cbbd266d9/eval.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /home/user/anaconda3/etc/profile.d/conda.sh
+#conda activate astro
+cd /home/user/projects
+
+export DEPENDENCY_KEY='dag-dependencies'
+
+echo "${DEPENDENCY_KEY}"
