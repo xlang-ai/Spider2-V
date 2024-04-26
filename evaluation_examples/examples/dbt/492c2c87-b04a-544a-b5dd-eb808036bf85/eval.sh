6b7b92f157154892fbfbf398951eb361557a8f22
ThisisXXZ <xxz.7203@gmail.com>
2024-04-26 17:47:41 +0800

    3 dbt samples modified, verbose instructions added

--- evaluation_examples/examples/dbt/492c2c87-b04a-544a-b5dd-eb808036bf85/eval.sh
+++ evaluation_examples/examples/dbt/492c2c87-b04a-544a-b5dd-eb808036bf85/eval.sh
@@ -6,12 +6,13 @@ cd ~/projects/jaffle_shop
 
 output=$(dbt run)
 flag1=$(echo $output | grep "Completed successfully")
-flag2=$(echo $output | grep -E "of 3 OK created sql view model .*\.stg_customers")
-flag3=$(echo $output | grep -E "of 3 OK created sql view model .*\.stg_orders")
-flag4=$(echo $output | grep -E "of 3 OK created sql table model .*\.customers")
+flag2=$(echo $output | grep -E "of 3 OK created sql .*\.stg_customers")
+flag3=$(echo $output | grep -E "of 3 OK created sql .*\.stg_orders")
+flag4=$(echo $output | grep -E "of 3 OK created sql .*\.customers")
 flag5=$(echo $output | grep "PASS=3")
 flag6=$(echo $output | grep "TOTAL=3")
-if [ -n "$flag1" ] && [ -n "$flag2" ] && [ -n "$flag3" ] && [ -n "$flag4" ] && [ -n "$flag5" ] && [ -n "$flag6" ]; then
+flag7=$(grep -E "ref" models/customers.sql)
+if [ -n "$flag1" ] && [ -n "$flag2" ] && [ -n "$flag3" ] && [ -n "$flag4" ] && [ -n "$flag5" ] && [ -n "$flag6" ] && [ -n "$flag7" ]; then
     echo "dbt run succeed"
 else
     echo "dbt run failed"
