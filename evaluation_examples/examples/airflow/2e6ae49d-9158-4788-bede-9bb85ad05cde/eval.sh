dc2fffe8b3d7b3d19e28d3d25169e5590cac13be
runoob <96536860+Hongcheng-Gao@users.noreply.github.com>
2024-04-29 15:01:20 +0800

    add cases

---
+++ evaluation_examples/examples/airflow/2e6ae49d-9158-4788-bede-9bb85ad05cde/eval.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /home/user/anaconda3/etc/profile.d/conda.sh
+#conda activate astro
+cd /home/user/projects
+
+export FILTER_SET='?_flt_0_first_name=Linda'
+export USER_STASTIC='userstatschartview'
+
+
+echo "${FILTER_SET}, ${USER_STASTIC}"
--- evaluation_examples/examples/airflow/3e8ddf14-a503-4ba1-83d0-32b2d91b33bb/eval.sh
+++ evaluation_examples/examples/airflow/3e8ddf14-a503-4ba1-83d0-32b2d91b33bb/eval.sh
@@ -4,8 +4,7 @@ source /home/user/anaconda3/etc/profile.d/conda.sh
 #conda activate astro
 cd /home/user/projects
 
-export TAB_SET='tab=audit_log'
-export OFFSET_KEY='offset=1'
+export LOGIN_KEY='login'
 
 
-echo "${TAB_SET}, ${OFFSET_KEY}"
+echo "${LOGIN_KEY}"
