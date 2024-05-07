1366fb3f99064a2be465ce5d642b20af97d701fb
rhythmcao <ruishengcao@gmail.com>
2024-05-07 23:40:58 +0800

    validate bigquery 16a9da77-2f40-455f-977e-32696ea97180

--- evaluation_examples/examples/bigquery/16a9da77-2f40-455f-977e-32696ea97180/init.sh
+++ evaluation_examples/examples/bigquery/16a9da77-2f40-455f-977e-32696ea97180/init.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+exec 2>/dev/null
+
 function to_ready_state(){
     echo "source /home/user/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
     echo "conda activate bigquery" >> ~/.bashrc
@@ -6,4 +10,4 @@ function to_ready_state(){
 to_ready_state
 
 gnome-terminal --working-directory=/home/user/projects/bigquery/project1
-code /home/user/projects/bigquery/project1
+code /home/user/projects/bigquery/project1/query.py
