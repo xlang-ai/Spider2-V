12efd061180f8a1399b1176f5c89d3fabc7ef510
lfy79001 <843265183@qq.com>
2024-05-07 23:46:07 +0800

    update bigquery snapshot_init.sh

--- evaluation_examples/examples/bigquery/snapshot_init.sh
+++ evaluation_examples/examples/bigquery/snapshot_init.sh
@@ -4,9 +4,8 @@ exec 2>/dev/null
 
 echo "password" | sudo -S apt install chromium-browser
 
-mkdir -p ~/projects/bigquery
-
 source /home/user/anaconda3/etc/profile.d/conda.sh  
 conda create -n bigquery python=3.11 -y >/dev/null 2>&1  
 conda activate bigquery
-pip install google-cloud-bigquery pandas db-dtypes
+
+pip install google-cloud-bigquery==3.21.0 pandas==2.2.2 db-dtypes==1.2.0
