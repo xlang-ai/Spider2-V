c4a3a4226622b9239e6c86e5ce6e7cf017309de0
lfy79001 <843265183@qq.com>
2024-05-07 22:58:39 +0800

    fix 16a9da77-2f40-455f-977e-32696ea97180

--- evaluation_examples/examples/bigquery/16a9da77-2f40-455f-977e-32696ea97180/init.sh
+++ evaluation_examples/examples/bigquery/16a9da77-2f40-455f-977e-32696ea97180/init.sh
@@ -1,9 +1,9 @@
 function to_ready_state(){
     echo "source /home/user/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
     echo "conda activate bigquery" >> ~/.bashrc
-    mkdir -p /home/user/projects/bigquery/project1
+    mkdir -p /home/user/projects/project1
 }
 to_ready_state
 
-gnome-terminal --working-directory=/home/user/projects/bigquery/project1
-code /home/user/projects/bigquery/project1
+gnome-terminal --working-directory=/home/user/projects/project1
+code /home/user/projects/project1
