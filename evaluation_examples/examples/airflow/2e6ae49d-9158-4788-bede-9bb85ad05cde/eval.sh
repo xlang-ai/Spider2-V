d093eb02aed6cadc181d74a390de57f0a9e03063
FredWuCZ <fredwucz@outlook.com>
2024-05-15 23:58:03 +0800

    Fixed 1 airflow example

--- evaluation_examples/examples/airflow/2e6ae49d-9158-4788-bede-9bb85ad05cde/eval.sh
+++ evaluation_examples/examples/airflow/2e6ae49d-9158-4788-bede-9bb85ad05cde/eval.sh
@@ -4,7 +4,7 @@ source /home/user/anaconda3/etc/profile.d/conda.sh
 #conda activate astro
 cd /home/user/projects
 
-export FILTER_SET='?_flt_0_first_name=Linda'
+export FILTER_SET='_first_name=Linda'
 export USER_STASTIC='userstatschartview'
 
 echo "${FILTER_SET}, ${USER_STASTIC}"
