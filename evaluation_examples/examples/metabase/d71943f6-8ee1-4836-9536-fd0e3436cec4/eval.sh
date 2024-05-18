a04801db4ec33dc1eb17aae97a959c07c9616ddd
zhanghanchong <308011618@qq.com>
2024-05-18 19:08:02 +0800

    add metabase example cc2

--- evaluation_examples/examples/metabase/d71943f6-8ee1-4836-9536-fd0e3436cec4/eval.sh
+++ evaluation_examples/examples/metabase/d71943f6-8ee1-4836-9536-fd0e3436cec4/eval.sh
@@ -2,7 +2,6 @@
 
 exec 2>/dev/null
 
-# check whether adding data from postgres database succeeds
 username=johnwilson@gmail.com
 password=Spider2.0
 session_id=$(curl -X POST http://localhost:3000/api/session -H 'Content-Type: application/json' -d "{\"username\": \"${username}\", \"password\": \"${password}\"}" | jq -rM .id)
