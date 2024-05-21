a92e2b8fe85982494514eeb98588de64481c7796
tsuky_chen <3107760494@qq.com>
2024-05-22 00:26:36 +0800

    update jupyter example 8259543a

--- desktop_env/evaluators/metrics/jupyterlab.py
+++ desktop_env/evaluators/metrics/jupyterlab.py
@@ -2,6 +2,7 @@
 import io
 import nbformat
 from typing import Dict, Any
+import json
 
 
 def is_jupyter_cell_executed(result: str, expected: Dict[str, Any], **options) -> float:
@@ -25,6 +26,29 @@ def is_jupyter_cell_executed(result: str, expected: Dict[str, Any], **options) -
         print(unexecuted)
         if unexecuted != expected['expected']:
             return 0.0
+        return 1.0
+    except Exception as e:
+        print(e)
+        return 0.0
+    
+def compare_ipynb_files(result: str, expected: str, **options) -> float:
+    """ Compare two Jupyter notebook files.
+    @args:
+        result: str - the path to the Jupyter notebook file.
+        expected: str - the path to the expected Jupyter notebook file.
+        options: Dict[str, Any] - the options.
+    """
+    try:
+        with io.open(result, 'r', encoding='utf-8') as f:
+            ipynb_file1 = json.load(f)
+        with io.open(expected, 'r', encoding='utf-8') as f:
+            ipynb_file2 = json.load(f)
+        
+        for (k1, v1), (k2, v2) in zip(ipynb_file1.items(), ipynb_file2.items()):
+            if k1 != k2:
+                return 0.0
+            if k1 != 'metadata' and v1 != v2:
+                return 0.0
         return 1.0
     except Exception as e:
         print(e)
