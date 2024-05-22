67945497684074f3d7bbf100c94028b8953eb4fb
rhythmcao <ruishengcao@gmail.com>
2024-05-22 14:07:05 +0800

    update dagster dependency

--- evaluation_examples/examples/spider2_snapshot.sh
+++ evaluation_examples/examples/spider2_snapshot.sh
@@ -155,7 +155,7 @@ setup_bigquery
 function setup_dagster() {
     conda create -n dagster python=3.11 -y >/dev/null 2>&1  
     conda activate dagster
-    pip install dagster==1.7.2 dagster-cloud==1.7.2 dagster-cloud-cli==1.7.2 dagster-graphql==1.7.2 dagster-pipes==1.7.2 dagster-webserver==1.7.2 dagster-airflow==1.7.2 dagster-airbyte==0.23.2 dagster-dbt==0.23.2 dagster-duckdb==0.23.2 dagster-duckdb-pandas==0.23.2 duckdb==0.10.2 dbt-duckdb==1.7.4 pandas==2.0.3 pytest==8.2.0 pyarrow==16.0.0 apache-airflow==2.9.0 apache-airflow-providers-mysql==5.5.4 mysqlclient==2.2.4 scikit-learn==1.4.2
+    pip install dagster==1.7.2 dagster-cloud==1.7.2 dagster-cloud-cli==1.7.2 dagster-graphql==1.7.2 dagster-pipes==1.7.2 dagster-webserver==1.7.2 dagster-airflow==1.7.2 dagster-airbyte==0.23.2 dagster-dbt==0.23.2 dagster-duckdb==0.23.2 dagster-duckdb-pandas==0.23.2 duckdb==0.10.2 dbt-duckdb==1.7.4 pandas==2.0.3 pytest==8.2.0 pyarrow==16.0.0 apache-airflow==2.9.0 apache-airflow-providers-mysql==5.5.4 mysqlclient==2.2.4 scikit-learn==1.4.2 notebook==7.1.2 matplotlib==3.8.4 seaborn==0.13.2 scipy==1.13.0 dagstermill==0.23.2 'papermill-origami>=0.0.8'
     mkdir -p /home/user/.dagster
     touch /home/user/.dagster/dagster.yaml
     echo "export DAGSTER_HOME=/home/user/.dagster" >> /home/user/.bashrc
