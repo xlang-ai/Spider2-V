e306b279fa8017868249750b8468d1d49e64c48c
rhythmcao <ruishengcao@gmail.com>
2024-05-24 00:22:08 +0800

    remove actino_info, too long

--- mm_agents/agent.py
+++ mm_agents/agent.py
@@ -432,9 +432,9 @@ class PromptAgent:
             })
 
         # action execution result of previous turn
-        if len(self.actions) > 0: # if has previous action list
-            infos = obs.get('infos', [])
-            self.add_action_infos(messages, self.actions[-1], infos)
+        # if len(self.actions) > 0: # if has previous action list
+            # infos = obs.get('infos', [])
+            # self.add_action_infos(messages, self.actions[-1], infos)
 
         # tackle the current observation
         if self.observation_space in ["screenshot", "screenshot_a11y_tree"]:
