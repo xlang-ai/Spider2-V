47ed21c81a9335cdc665624fbb517bacfeed1d32
fyq5166 <125123667+fyq5166@users.noreply.github.com>
2024-05-27 13:29:54 +0800

    Airflow hard(>15) examples

---
+++ evaluation_examples/examples/airflow/d0b56374-ba88-4b18-9da3-f50df7f4d639/init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ASTRO_RUNTIME_VERSION=10.5.0
+
+function to_ready_state(){
+    kill -9 $(lsof -t -i:5000)
+    echo "source /home/user/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
+    echo "conda activate airflow" >> ~/.bashrc
+    cd /home/user/projects
+    mv /home/user/mlflow.zip .
+    chmod a+x mlflow.zip  
+    unzip -q mlflow.zip  
+    rm -rf mlflow.zip 
+    cd /home/user/projects/mlflow
+    echo -e "y\n" | astro dev init
+    rm -rf /home/user/projects/mlflow/dags/exampledag.py
+    sed -i "s/astro-runtime:.*$/astro-runtime:${ASTRO_RUNTIME_VERSION}/" Dockerfile
+    code --user-data-dir=/home/user/projects/mlflow
+    astro dev start --no-browser >/dev/null 2>&1
+    wait
+}
+to_ready_state
+
+gnome-terminal --working-directory=/home/user/Downloads
+gnome-terminal --working-directory=/home/user/projects/mlflow
+code /home/user/projects/mlflow/dags/predict.py
+
+
+
+
+
