d7206d8fbc5de0ee400c04259e8ce97192e226be
lfy79001 <843265183@qq.com>
2024-05-29 12:59:18 +0800

    add example 58013f69-4764-4c9b-972f-5db3010595d2

---
+++ evaluation_examples/examples/jupyter/58013f69-4764-4c9b-972f-5db3010595d2/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+####################################################################################################
+# Please ensure that Chromium or Chrome, VSCode, docker and anaconda3 is installed on your system before running this script.
+# The installed anaconda3 should be in the directory /home/user/anaconda3/.
+# Some images should be pre-downloaded in VM snapshots to accelerate the process.
+# Please ensure the initial project is copied to the home directory.
+# This script is tested on Ubuntu 22.04 LTS.
+####################################################################################################
+
+# ignore all output and error
+exec 1>/dev/null
+exec 2>/dev/null
+
+source /home/user/anaconda3/etc/profile.d/conda.sh
+conda activate jupyter
+echo "source /home/user/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
+echo "conda activate jupyter" >> ~/.bashrc
