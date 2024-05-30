02269a7d80dcc5141b39103d245054a07ca92638
runoob <96536860+Hongcheng-Gao@users.noreply.github.com>
2024-05-30 20:02:55 +0800

    reduce database

--- evaluation_examples/examples/superset/6749d85d-b57c-4f27-9b2d-8c0d12658cf2/init.sh
+++ evaluation_examples/examples/superset/6749d85d-b57c-4f27-9b2d-8c0d12658cf2/init.sh
@@ -38,6 +38,50 @@ function start_superset_server() {
 
 start_superset_server
 
+cd /home/user
+docker cp all_databases.sql superset_db:/all_databases.sql
+docker exec -t superset_db psql -U superset -f /all_databases.sql
+
+function init_data() {
+    token=$(curl -X POST "http://localhost:8088/api/v1/security/login" \
+        -H "Content-Type: application/json" \
+        -d '{
+            "username": "admin",
+            "password": "admin",
+            "provider": "db"
+        }' | jq -rM ".access_token")
+
+    charts=$(curl -X GET "http://localhost:8088/api/v1/chart/" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer ${token}")
+
+    chart_ids=$(echo $charts | jq -r '.result[].id')
+
+    for id in $chart_ids; do
+        curl -X DELETE "http://localhost:8088/api/v1/chart/${id}" \
+            -H "Content-Type: application/json" \
+            -H "Authorization: Bearer ${token}"
+    done
+
+
+    dashboards=$(curl -X GET "http://localhost:8088/api/v1/dashboard/" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer ${token}")
+
+
+    dashboard_ids=$(echo $dashboards | jq -r '.result[].id')
+
+
+    for id in $dashboard_ids; do
+        curl -X DELETE "http://localhost:8088/api/v1/dashboard/${id}" \
+            -H "Content-Type: application/json" \
+            -H "Authorization: Bearer ${token}"
+    done
+}
+
+init_data
+
+
 function create_databoard() {
     cd /home/user/
     token=$(curl -X POST "http://localhost:8088/api/v1/security/login" \
