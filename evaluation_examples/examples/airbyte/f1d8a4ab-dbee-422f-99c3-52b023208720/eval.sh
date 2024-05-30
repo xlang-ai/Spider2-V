43c9d7161762456c410bc20adc2bae829f8702eb
yuchen <viveutvivasmyc@gmail.com>
2024-05-30 22:40:18 +1000

    update 1 airbyte sample.

--- evaluation_examples/examples/airbyte/f1d8a4ab-dbee-422f-99c3-52b023208720/eval.sh
+++ evaluation_examples/examples/airbyte/f1d8a4ab-dbee-422f-99c3-52b023208720/eval.sh
@@ -5,13 +5,13 @@ exec 2>/dev/null
 # source /home/user/anaconda3/etc/profile.d/conda.sh
 # conda activate airbyte
 
-expected_result="1|Eg1 IncApp 2|Eg2b IncAp"
-
+expected_result="1|Eg1 IncApp 2|Eg2a IncAp 2|Eg2b IncAp"
 actual_result=$(docker exec -i airbyte-destination psql -U postgres -d postgres -t -A -F"|" -c "SELECT id, name FROM public.table_one")
 actual_result_clean=$(echo "$actual_result" | sed '/^$/d' | xargs)
 expected_result_clean=$(echo "$expected_result" | sed '/^$/d' | xargs)
 
-if [ "$actual_result_clean" == "$expected_result_clean" ]; then
+
+if [ "${actual_result_clean}" == "${expected_result_clean}" ]; then
     echo "Check table in destination, succeed"
     exit 0
 fi
