00efda212f52a04642b7b067647111dd80001392
Hongcheng Gao <96536860+Hongcheng-Gao@users.noreply.github.com>
2024-05-31 20:42:55 +0800

    Update eval.sh

--- evaluation_examples/examples/superset/c8d2733e-738d-43dd-bbae-c6401743737d/eval.sh
+++ evaluation_examples/examples/superset/c8d2733e-738d-43dd-bbae-c6401743737d/eval.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 exec 2>/dev/null
-export USER_KEY='(label:%27Superset%20Admin%27,value:1)'
-export TIME_KEY='(1714551823000,1717143832000)'
 
-echo "${USER_KEY}, ${TIME_KEY}"
+export USER_KEY='(label:%27Superset%20Admin%27,value:1)'
+export START_TIME_KEY='(1714551823'
+export END_TIME_KEY='1717143832'
+
+echo "${USER_KEY}, ${START_TIME_KEY}, ${END_TIME_KEY}"
