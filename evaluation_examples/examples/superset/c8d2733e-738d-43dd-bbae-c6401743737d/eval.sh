2f9335b0c0129e49f392c1d4ec6e2daadfa1eae6
rhythmcao <ruishengcao@gmail.com>
2024-06-13 21:37:34 +0800

    delete 9 examples, not validated, but in examples_directory

--- evaluation_examples/examples/superset/c8d2733e-738d-43dd-bbae-c6401743737d/eval.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-exec 2>/dev/null
-
-export USER_KEY='(label:%27Superset%20Admin%27,value:1)'
-export START_TIME_KEY='(1714551823'
-export END_TIME_KEY='1717143832'
-
-echo "${USER_KEY}, ${START_TIME_KEY}, ${END_TIME_KEY}"
